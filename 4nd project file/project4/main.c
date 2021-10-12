0803a62c9480a0f8b782abdf6eb2618ea0afabf0
Leejaechul <avnljc@naver.com>
2021-10-12 16:51:14 +0900

    4과제 all upload

--- 4nd project file/project4/main.c
+++ 4nd project file/project4/main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-double dEuclideanDistance(double nRandomX1, double nRandomY1, double nRandomX2, double nRandomY2);
+double dEuclideanDistance(double X1, double Y1, double X2, double Y2); //유클리디안 거리 함수 선언
 
 int main(void)
 {
-	double dRandomX1 = 0.0, dRandomX2 = 0.0, dRandomY1 = 0.0, dRandomY2 = 0.0;
-
+	double dRandomX1 = 0.0, dRandomX2 = 0.0, dRandomY1 = 0.0, dRandomY2 = 0.0; //실수형으로 변수값 초기화
+	
 	printf("임의의 한 점의 좌표를 입력(x,y 좌표-실수값) : ");
-	scanf("%d%d", &dRandomX1, &dRandomY1);
+	scanf("%lf%lf", &dRandomX1, &dRandomY1);
 	printf("임의의 한 점의 좌표를 입력(x,y 좌표-실수값) : ");
-	scanf("%d%d", &dRandomX2, &dRandomY2);
-	printf("두 점사이의 거리 : %lf" ,dEuclideanDistance(dRandomX1, dRandomY1, dRandomX2, dRandomY2));
-
+	scanf("%lf%lf", &dRandomX2, &dRandomY2);
+	printf("두 점사이의 거리 : %lf\n", dEuclideanDistance(dRandomX1, dRandomY1, dRandomX2, dRandomY2)); //유클리디안 함수 호출
+	
 	system("pause");
 	return 0;
 }
@@ -24,6 +24,6 @@ double dEuclideanDistance(double X1, double Y1, double X2, double Y2)
 	dXvalue = (X1 - X2) * (X1 - X2);
 	dYvalue = (Y1 - Y2) * (Y1 - Y2);
 	calculation = dXvalue + dYvalue;
-	res = sqrt(calculation);
+	res = sqrt(calculation); //제곱값 함수 호출
 	return res;
 }
