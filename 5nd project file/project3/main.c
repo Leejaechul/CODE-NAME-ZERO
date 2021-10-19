7737301591cc20d44f24f031c409a3cfab55cf54
Leejaechul <avnljc@naver.com>
2021-10-19 21:28:37 +0900

    10/19 21:28 upload

--- 5nd project file/project3/main.c
+++ 5nd project file/project3/main.c
@@ -3,14 +3,14 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define END_CONDITION 7 //define 으로 숫자에 의미 부여
-#define first_side 0
-#define second_side 1
-#define third_side 2
-#define fourth_side 3
-#define fifth_side 4
-#define sixth_side 5
-#define RandNum_Range 6
+#define END_CONDITION 999 // 프로그램 종료 상수 define
+#define first_side 0	// 첫번째 면의 상수 define
+#define second_side 1	// 두번째 면의 상수 define
+#define third_side 2	// 세번째 면의 상수 define
+#define fourth_side 3	// 네번째 면의 상수 define
+#define fifth_side 4	// 다섯번째 면의 상수 define
+#define sixth_side 5	// 여섯번째 면의 상수 define
+#define RandNum_Range 6	// 랜덤넘버 함수의 범위 값 define
 
 void TimeRandomSeed();
 unsigned int nGenRandomNum(nRange);
@@ -20,55 +20,64 @@ int main(void)
 {
 	int nRange = 0, nN = 0, i = 0;
 	int RandNumRes = 0;
-	int Sum_SideNumber = 0;
 	int Count_SideNumber1 = 0, Count_SideNumber2 = 0, Count_SideNumber3 = 0, Count_SideNumber4 = 0, Count_SideNumber5 = 0, Count_SideNumber6 = 0;
-	int Nth_side = 0;
 	int side_1 = 0, side_2 = 0, side_3 = 0, side_4 = 0, side_5 = 0, side_6 = 0;
+	char Percent = '%';		// % 기호를 출력하기위해 char형 문자 선언
 	TimeRandomSeed();
-	printf("주사위를 몇번 던질까요? : ");
-	scanf_s("%d", &nN);
-	for (i = 0; i < nN; i++) {
-		RandNumRes = nGenRandomNum(RandNum_Range);
-		switch (RandNumRes) {
-		case first_side:
-			side_1 += 1;
-			break;
-		case second_side:
-			side_2 += 2;
-			break;
-		case third_side:
-			side_3 += 3;
-			break;
-		case fourth_side:
-			side_4 += 4;
-			break;
-		case fifth_side:
-			side_5 += 5;
-			break;
-		case sixth_side:
-			side_6 += 6;
-			break;
+	while (1) {
+		printf("주사위를 몇번 던질까요? \n프로그램을 종료하려면 999를 입력하세요\n");
+		scanf_s("%d", &nN);
+		if (nN == END_CONDITION) {
+			return 0;
+		}
+		else if (nN <= 0) {
+			printf("잘못된 값을 입력하셨습니다. 다시 입력해 주세요\n\n");
+			continue;
+		}
+		else {
+			for (i = 0; i < nN; i++) {
+				RandNumRes = nGenRandomNum(RandNum_Range);
+				switch (RandNumRes) {		// swich ~ case 문으로 반환된 값을 모두 저장
+				case first_side:
+					side_1 += 1;
+					break;
+				case second_side:
+					side_2 += 2;
+					break;
+				case third_side:
+					side_3 += 3;
+					break;
+				case fourth_side:
+					side_4 += 4;
+					break;
+				case fifth_side:
+					side_5 += 5;
+					break;
+				case sixth_side:
+					side_6 += 6;
+					break;
+				}
+			}
+			Count_SideNumber1 = Cal_Count_SideN(side_1, 1);			// 더한수를 일정한 값으로 나누어 횟수 도출
+			Count_SideNumber2 = Cal_Count_SideN(side_2, 2);
+			Count_SideNumber3 = Cal_Count_SideN(side_3, 3);
+			Count_SideNumber4 = Cal_Count_SideN(side_4, 4);
+			Count_SideNumber5 = Cal_Count_SideN(side_5, 5);
+			Count_SideNumber6 = Cal_Count_SideN(side_6, 6);
+			printf("1번 면이 나온 횟수 : %d\n", Count_SideNumber1);
+			printf("1번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber1 / (double)nN * 100), Percent);	// 계산한 횟수로 확률 도출
+			printf("2번 면이 나온 횟수 : %d\n", Count_SideNumber2);
+			printf("2번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber2 / (double)nN * 100), Percent);
+			printf("3번 면이 나온 횟수 : %d\n", Count_SideNumber3);
+			printf("3번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber3 / (double)nN * 100), Percent);
+			printf("4번 면이 나온 횟수 : %d\n", Count_SideNumber4);
+			printf("4번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber4 / (double)nN * 100), Percent);
+			printf("5번 면이 나온 횟수 : %d\n", Count_SideNumber5);
+			printf("5번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber5 / (double)nN * 100), Percent);
+			printf("6번 면이 나온 횟수 : %d\n", Count_SideNumber6);
+			printf("6번 면이 나온 확률 : %.1lf%c\n\n", ((double)Count_SideNumber6 / (double)nN * 100), Percent);
 		}
 	}
-	Count_SideNumber1 = Cal_Count_SideN(side_1, 1);
-	Count_SideNumber2 = Cal_Count_SideN(side_2, 2);
-	Count_SideNumber3 = Cal_Count_SideN(side_3, 3);
-	Count_SideNumber4 = Cal_Count_SideN(side_4, 4);
-	Count_SideNumber5 = Cal_Count_SideN(side_5, 5);
-	Count_SideNumber6 = Cal_Count_SideN(side_6, 6);
-	printf("1번 면이 나온 횟수 : %d\n", Count_SideNumber1);
-	printf("1번 면이 나온 확률 : %d\n\n", Count_SideNumber1 / nN * 100);
-	printf("2번 면이 나온 횟수 : %d\n", Count_SideNumber2);
-	printf("2번 면이 나온 확률 : %d\n\n", Count_SideNumber2 / nN * 100);
-	printf("3번 면이 나온 횟수 : %d\n", Count_SideNumber3);
-	printf("3번 면이 나온 확률 : %d\n\n", Count_SideNumber3 / nN * 100);
-	printf("4번 면이 나온 횟수 : %d\n", Count_SideNumber4);
-	printf("4번 면이 나온 확률 : %d\n\n", Count_SideNumber4 / nN * 100);
-	printf("5번 면이 나온 횟수 : %d\n", Count_SideNumber5);
-	printf("5번 면이 나온 확률 : %d\n\n", Count_SideNumber5 / nN * 100);
-	printf("6번 면이 나온 횟수 : %d\n", Count_SideNumber6);
-	printf("6번 면이 나온 확률 : %d\n\n", Count_SideNumber6 / nN * 100);
-
 
 	system("pause");
 	return 0;
@@ -89,7 +98,7 @@ unsigned int nGenRandomNum(int nRange) // n까지의 범위만큼 랜덤숫자를 뽑고 싶다�
 	return nRandomNum;
 }
 
-int Cal_Count_SideN(int side_N, int Divide_Num) {
+int Cal_Count_SideN(int side_N, int Divide_Num) { // 면의 번호와 그에 맞게 나누어 줄 매개변수값으로 정확한 횟수 도출 함수 정의
 	int Calculation = 0;
 	Calculation = side_N / Divide_Num;
 	return Calculation;
